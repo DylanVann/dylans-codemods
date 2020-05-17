f0d2c58ebea1b767cebdf17bd61a76e01e1c8541
Dylan Vann <dylan@dylanvann.com>
2020-05-16 23:23:42 -0400

    use inlined get, so the codemod can be used by url

--- transforms/babel-plugin-jsx-control-statements-to-javascript.js
+++ transforms/babel-plugin-jsx-control-statements-to-javascript.js
@@ -1,4 +1,14 @@
-import _ from 'lodash'
+// https://gist.github.com/jeneg/9767afdcca45601ea44930ea03e0febf
+const get = (obj, path, def) =>
+  (() =>
+    typeof path === 'string'
+      ? path.replace(/\[(\d+)]/g, '.$1')
+      : path.join('.'))()
+    .split('.')
+    .filter(Boolean)
+    .every((step) => (obj = obj[step]) !== undefined)
+    ? obj
+    : def
 
 /**
  * This codemod converts code written with "babel-plugin-jsx-control-statements"
@@ -8,15 +18,15 @@ export default function transformer(file, api) {
   const j = api.jscodeshift
   return j(file.source)
     .find(j.JSXElement)
-    .filter((value) => _.get(value, 'value.openingElement.name.name') === 'If')
+    .filter((value) => get(value, 'value.openingElement.name.name') === 'If')
     .forEach((path) => {
-      const children = _.get(path, 'value.children')
+      const children = get(path, 'value.children')
       const isEmptyText = (child) =>
         typeof child.value === 'string' && child.value.trim() === ''
       const filteredChildren = children.filter((v) => !isEmptyText(v))
-      const attributes = _.get(path, 'value.openingElement.attributes')
+      const attributes = get(path, 'value.openingElement.attributes')
       const conditionAttribute = attributes.find(
-        (v) => _.get(v, 'name.name') === 'condition',
+        (v) => get(v, 'name.name') === 'condition',
       )
       const condition = conditionAttribute.value.expression
       const left = j.callExpression(j.identifier('Boolean'), [condition])
@@ -29,7 +39,7 @@ export default function transformer(file, api) {
               children,
             )
       let expression = j.logicalExpression('&&', left, right)
-      const isInsideJsx = _.get(path, 'parentPath.name') === 'children'
+      const isInsideJsx = get(path, 'parentPath.name') === 'children'
       if (isInsideJsx) {
         expression = j.jsxExpressionContainer(expression)
       }
